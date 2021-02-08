ddcf251c7cbdacb79e62532e2c93066f19decfaf
Andrey <freeman49@inbox.ru>
2021-02-08 13:04:28 +0800

    step 9

--- src/js/script.js
+++ src/js/script.js
@@ -151,6 +151,8 @@ $(document).ready(function () {
 		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
 		return false;
 	});
+
+	new WOW().init();
 });
 
 // const slider = tns({
