7f4d9930a44343d122add2527c9cbe37755d2bb0
Andrey <freeman49@inbox.ru>
2021-02-08 10:54:02 +0800

    step 8

--- src/js/script.js
+++ src/js/script.js
@@ -1,107 +1,156 @@
 $(document).ready(function () {
-    $('.carousel__inner').slick({
-        infinite: true,
-        speed: 1300,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/row-left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/row-right.png"></button>',
-        responsive: [{
-                breakpoint: 992,
-                settings: {
-                    arrows: false,
-                    infinite: true,
-                    dots: true
-                }
-            }
+	$(".carousel__inner").slick({
+		infinite: true,
+		speed: 1300,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="icons/row-left.png"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="icons/row-right.png"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					infinite: true,
+					dots: true,
+				},
+			},
+		],
+	});
 
-        ]
+	$("ul.catalog__tabs").on(
+		"click",
+		"li:not(.catalog__tab_active)",
+		function () {
+			$(this)
+				.addClass("catalog__tab_active")
+				.siblings()
+				.removeClass("catalog__tab_active")
+				.closest("div.container")
+				.find("div.catalog__content")
+				.removeClass("catalog__content_active")
+				.eq($(this).index())
+				.addClass("catalog__content_active");
+		}
+	);
 
-    });
+	// $('.catalog-item__link').each(function (i) {
+	//     $(this).on('click', function (e) {
+	//         e.preventDefault();
+	//         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+	//         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+	//     });
 
+	// });
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
-        $(this)
-            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-    });
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on("click", function (e) {
+				e.preventDefault();
+				$(".catalog-item__content")
+					.eq(i)
+					.toggleClass("catalog-item__content_active");
+				$(".catalog-item__list")
+					.eq(i)
+					.toggleClass("catalog-item__list_active");
+			});
+		});
+	}
 
-    // $('.catalog-item__link').each(function (i) {
-    //     $(this).on('click', function (e) {
-    //         e.preventDefault();
-    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //     });
+	toggleSlide(".catalog-item__link");
+	toggleSlide(".catalog-item__back");
 
-    // });
+	//Modal
+	$("[data-modal=consultation]").on("click", function () {
+		$(".overlay, #consultation").fadeIn("slow");
+	});
 
-    function toggleSlide(item) {
-        $(item).each(function (i) {
-            $(this).on('click', function (e) {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            });
+	$(".modal__close").on("click", function () {
+		$(".overlay, #consultation, #thanks, #order").fadeOut("slow");
+	});
 
-        });
-    }
+	$(".button_mini").each(function (i) {
+		$(this).on("click", function () {
+			$("#order .modal__descr").text(
+				$(".catalog-item__subtitle").eq(i).text()
+			);
+			$(".overlay, #order").fadeIn("slow");
+		});
+	});
 
-    toggleSlide('.catalog-item__link');
-    toggleSlide('.catalog-item__back');
+	function valideForm(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2,
+				},
+				email: {
+					required: true,
+					email: true,
+				},
+				phone: {
+					required: true,
+					minlength: 6,
+				},
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста введите свое имя",
+					minlength: jQuery.validator.format(
+						"Необходимо не менее {0} символов!"
+					),
+				},
+				email: {
+					required: "Пожалуйста введите свою почту",
+					email: "Пожалуйста введите почту в формате name@domain.com",
+				},
+				phone: {
+					required: "Пожалуйста введите номер телефона",
+					minlength: jQuery.validator.format(
+						"Необходимо не менее {0} символов!"
+					),
+				},
+			},
+		});
+	}
 
-    //Modal
-    $('[data-modal=consultation]').on('click', function () {
-        $('.overlay, #consultation').fadeIn('slow');
-    });
+	valideForm("#consultation form");
+	valideForm("#consultation-form");
+	valideForm("#order form");
 
-    $('.modal__close').on('click', function () {
-        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-    });
+	$("input[name=phone]").mask("+7(999) 999-99-99");
 
-    $('.button_mini').each(function (i) {
-        $(this).on('click', function () {
-            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn('slow');
-        });
-    });
+	$("form").submit(function (e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize(),
+		}).done(function () {
+			$(this).find("input").val("");
+			$("#consultation, #order").fadeOut();
+			$(".overlay, #thanks").fadeIn("slow");
 
-    function valideForm(form) {
-        $(form).validate({
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 2
-                },
-                email: {
-                    required: true,
-                    email: true
-                },
-                phone: {
-                    required: true,
-                    minlength: 6
-                }
-            },
-            messages: {
-                name: {
-                    required: "Пожалуйста введите свое имя",
-                    minlength: jQuery.validator.format("Необходимо не менее {0} символов!")
-                },
-                email: {
-                    required: "Пожалуйста введите свою почту",
-                    email: "Пожалуйста введите почту в формате name@domain.com"
-                },
-                phone: {
-                    required: "Пожалуйста введите номер телефона",
-                    minlength: jQuery.validator.format("Необходимо не менее {0} символов!")
-                }
-            }
-        });
-    }
+			$("form").trigger("reset");
+		});
+		return false;
+	});
 
-    valideForm('#consultation form');
-    valideForm('#consultation-form');
-    valideForm('#order form');
+	//Scroll and pageup
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$(".pageup").fadeIn();
+		} else {
+			$(".pageup").fadeOut();
+		}
+	});
 
-
-
+	$("a[href^='#']").click(function () {
+		const _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+		return false;
+	});
 });
 
 // const slider = tns({
